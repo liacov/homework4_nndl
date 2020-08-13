d383ab4d6b5a65a23ff56610f8652ebed91652e8
liacov <lauraiacovissi@gmail.com>
2020-08-13 16:34:01 +0200

    basic experiments

---
+++ autoencoder.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import os
+import torch
+import random
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from tqdm import tqdm
+
+
+#%% Define the network architecture
+    
+class Autoencoder(nn.Module):
+    
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        
+        ### Encoder
+        self.encoder_cnn = nn.Sequential(
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            nn.ReLU(True)
+        )
+        self.encoder_lin = nn.Sequential(
+            nn.Linear(3 * 3 * 32, 64),
+            nn.ReLU(True),
+            nn.Linear(64, encoded_space_dim)
+        )
+        
+        ### Decoder
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(encoded_space_dim, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.decode(x)
+        return x
+
+    def encode(self, x):
+        # Apply convolutions
+        x = self.encoder_cnn(x)
+        # Flatten
+        x = x.view([x.size(0), -1])
+        # Apply linear layers
+        x = self.encoder_lin(x)
+        return x
+    
+    def decode(self, x):
+        # Apply linear layers
+        x = self.decoder_lin(x)
+        # Reshape
+        x = x.view([-1, 32, 3, 3])
+        # Apply transposed convolutions
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+
+### Training function
+def train_epoch(net, dataloader, loss_fn, optimizer, device):
+    # Training
+    net.train()
+    for sample_batch in dataloader:
+        # Extract data and move tensors to the selected device
+        image_batch = sample_batch[0].to(device)
+        # Forward pass
+        output = net(image_batch)
+        loss = loss_fn(output, image_batch)
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # Print loss
+        print('\t partial train loss: %f' % (loss.data))
+
+
+### Testing function
+def test_epoch(net, dataloader, loss_fn, optimizer, device):
+    # Validation
+    net.eval() # Evaluation mode (e.g. disable dropout)
+    with torch.no_grad(): # No need to track the gradients
+        conc_out = torch.Tensor().float()
+        conc_label = torch.Tensor().float()
+        for sample_batch in dataloader:
+            # Extract data and move tensors to the selected device
+            image_batch = sample_batch[0].to(device)
+            # Forward pass
+            out = net(image_batch)
+            # Concatenate with previous outputs
+            conc_out = torch.cat([conc_out, out.cpu()])
+            conc_label = torch.cat([conc_label, image_batch.cpu()]) 
+        # Evaluate global loss
+        val_loss = loss_fn(conc_out, conc_label)
+    return val_loss.data
